1f0116ada701007ccc3eff77f64e2482e87b8489
znandev <nannozeta@gmail.com>
2026-08-13 23:53:00 -0400

    feat: refactor telegram notification system and fixes bugs #4

--- state.py
+++ state.py
@@ -1,10 +1,13 @@
 last_seen = 0
+
 power_state = "UNKNOWN"
-countdown = 0
-countdown_running = False
+
 device_name = "UNKNOWN"
 firmware = "UNKNOWN"
 model = "UNKNOWN"
 esp_ip = "UNKNOWN"
 
 esp_seen_once = False
+
+countdown = 0
+countdown_running = False
